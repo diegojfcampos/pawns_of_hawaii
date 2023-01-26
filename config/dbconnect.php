cda8a29d542d30d48d12cb0acf0ed96c8000b76c
diegojfcampos <diegojfcampos@gmail.com>
2023-01-26 20:38:10 +0000

    updated db

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -6,7 +6,7 @@ $dbname = 'pawnsofhawaii-database';
 
 //Creating connection
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/home/diego/Downloads/DigiCertGlobalRootCA.crt (2).pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "/home/diego/Downloads/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $host, $user, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
