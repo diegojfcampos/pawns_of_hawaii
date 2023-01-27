f7ec90a6fe377083e8a43c7345d6aa5e32e9fa1b
diegojfcampos <diegojfcampos@gmail.com>
2023-01-27 19:59:58 +0000

    Updated azure db configs

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -4,9 +4,10 @@ $user = 'fznhknnefq';
 $password = 'admin@123';
 $dbname = 'pawnsofhawaii-database';
 
-echo "Trying to connect";
+echo  "Trying to connect";
 //Creating connection
-
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "../DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $host, $user, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
