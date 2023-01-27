5e70c748cb53dc5e373e22da588c330573f90435
diegojfcampos <diegojfcampos@gmail.com>
2023-01-27 00:57:41 +0000

    Updated azure db configs

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -7,7 +7,7 @@ $dbname = 'pawnsofhawaii-database';
 
 //Creating connection
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/home/diego/Downloads/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "/home/diego/Desktop/paws_of_hawaii/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $host, $user, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
