c20104317994c463779627484e241f6c70112c1c
diegojfcampos <diegojfcampos@gmail.com>
2023-01-28 07:08:54 +0000

    Included Find a pawan edit table

--- controller/dologin.php
+++ controller/dologin.php
@@ -34,7 +34,8 @@
             //Starting Session                
             session_start();
             $hashed_value = hash("sha256", $value);
-            setcookie("hashed_value", $hashed_value, time() + (86400 * 30), "/");
+            setcookie("hashed_value", $hashed_value, time() + (12 * 60 * 60), "/");
+            
             //Instancing user data into Super Global Session
             session_regenerate_id();
             $_SESSION['id'] = $user["userid"];            
