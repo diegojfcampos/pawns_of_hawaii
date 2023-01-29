c4b17ab8fbd06aa20b9dc9ae5e5376a7f320b7f2
diegojfcampos <diegojfcampos@gmail.com>
2023-01-29 17:44:02 +0000

    fixing header signup

--- controller/dosignup.php
+++ controller/dosignup.php
@@ -64,7 +64,8 @@ require_once('../config/dbconnect.php');
                 session_start();
                 $_SESSION['email'] = $email;
                 $_SESSION['privileges'] = $privileges;  
-                header('Location: ./views/index.php');             
+                header('Location: ../index.php');  
+                echo "User inserted after";           
              
                 
 
